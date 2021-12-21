9843207e0022086a5763c3e260ece50bb7812ba0
Morteza Khoramdel <mortezakhoramdeldreadful@gmail.com>
2021-12-21 14:28:49 +0330

    add access

--- authentication-and-authorization/app/AccessControl/Controllers/ControllerAccessControl.js
+++ authentication-and-authorization/app/AccessControl/Controllers/ControllerAccessControl.js
@@ -3,20 +3,7 @@ let model = new ModelAccessControl()
 const string = require('../../../assets/strings')
 
 class ControllerAccessControl {
-    async checkAccess(req, res) {
-        try {
-            let resData = await model.checkAccess(req.data.resource, req.data.role, req.data.operation)
-            if (resData.rowCount) {
-                res.statusCode = 200
-                res.end(string.RESPONSE_ACCEPT_ACCESS_CONTROL);
-            } else {
-                res.statusCode = 404
-                res.end(string.RESPONSE_ERROR_ACCESS_CONTROL);
-            }
-        } catch (e) {
-            console.log(e)
-        }
-    }
+
 
     async assignAccess(req, res) {
         try {

9a4e2e331ad83464490191836c0a5ac752e68b28
Amirhossein Torkanloo <torkanlooa@gmail.com>
2021-12-21 15:03:39 +0330

    new command

--- authentication-and-authorization/command.sh
+++ authentication-and-authorization/command.sh
@@ -1,1 +1,3 @@
+/bin/bash
+sudo docker run -p 8000:8000 -v /edit-this-path-to/config:/config  --network authentication-and-authorization-network --name authentication-and-authorization-backend --rm authentication-and-authorization-backend
 

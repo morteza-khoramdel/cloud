3320273ebb71914e8e0b6826c36bdb0245a1c4ef
Morteza Khoramdel <mortezakhoramdeldreadful@gmail.com>
2021-12-21 14:23:40 +0330

    remove  models data

--- analytical_service/api/models.py
+++ analytical_service/api/models.py
@@ -5,7 +5,14 @@ class VGSales(models.Model):
     rank = models.AutoField(primary_key=True, db_column="Rank")
     name = models.TextField(db_column='Name')
     platform = models.CharField(max_length=16, null=True, blank=True, db_column="Platform")
-
+    year = models.PositiveIntegerField(null=True, blank=True, db_column="Year")
+    genre = models.CharField(max_length=25, null=True, blank=True, db_column='Genre')
+    publisher = models.CharField(max_length=50, null=True, blank=True, db_column='Publisher')
+    na_sales = models.FloatField(db_column='NA_Sales')
+    eu_sales = models.FloatField(db_column='EU_Sales')
+    jp_sales = models.FloatField(db_column="JP_Sales")
+    other_sales = models.FloatField(db_column='Other_Sales')
+    global_sales = models.FloatField(db_column='Gloabl_Sales')
 
     class Meta:
         managed=True

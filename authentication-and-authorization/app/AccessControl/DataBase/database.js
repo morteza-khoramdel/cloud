9e65ee4a9381a24ecf926b59cdf1db39639888c0
Morteza Khoramdel <mortezakhoramdeldreadful@gmail.com>
2021-12-21 14:40:59 +0330

    env

--- authentication-and-authorization/app/AccessControl/DataBase/database.js
+++ authentication-and-authorization/app/AccessControl/DataBase/database.js
@@ -2,7 +2,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const {Pool, Client} = require('pg');
 const pool = new Pool({
     user: process.env.USER_DATABASE,
-    host: process..HOST_DATABASE_AUTH,
+    host: process.env.HOST_DATABASE_AUTH,
     database: process.env.NAME_DATABASE,
     password: process.env.PASSWORD_DATABASE,
     port: process.env.PORT_DATABASE_AUTH,
